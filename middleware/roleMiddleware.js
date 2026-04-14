fc4c35ace74179e12ba12786815cc9ce8e51befa
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-15 00:58:25 +0100

    Add role-based access control middleware

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,1 +1,11 @@
+const roleMiddleware = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({ msg: 'Access denied' });
+    }
+    next();
+  };
+};
+
+module.exports = roleMiddleware;
 
