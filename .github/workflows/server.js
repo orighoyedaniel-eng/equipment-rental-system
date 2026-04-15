3f4bda2252de961982f42b4202daab4c7feeb7bb
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-15 01:05:21 +0100

    Delete .github/workflows directory

--- .github/workflows/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Equipment Rental System API running...');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
