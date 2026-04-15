8659ceca5953da85ae580dd7663a0c1190d311c7
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-15 08:40:00 +0000

    save update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: '',
   database: process.env.DB_NAME
 });
 
