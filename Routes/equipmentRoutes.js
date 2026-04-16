f34b4485e6b5f7a0703d0c27547164eda37d8bb2
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-16 23:53:38 +0100

    Update equipmentRoutes.js

--- Routes/equipmentRoutes.js
+++ Routes/equipmentRoutes.js
@@ -1,7 +1,22 @@
+// routes/equipment.js
+
 const express = require('express');
 const router = express.Router();
+const equipmentController = require('../controllers/equipmentController');
 
-router.get('/', (req, res) => res.send('Equipment routes working'));
+// ➡️ Get all equipment
+router.get('/', equipmentController.getAllEquipment);
+
+// ➡️ Get equipment by ID
+router.get('/:id', equipmentController.getEquipmentById);
+
+// ➡️ Add new equipment
+router.post('/', equipmentController.addEquipment);
+
+// ➡️ Update equipment
+router.put('/:id', equipmentController.updateEquipment);
+
+// ➡️ Delete equipment
+router.delete('/:id', equipmentController.deleteEquipment);
 
 module.exports = router;
-
