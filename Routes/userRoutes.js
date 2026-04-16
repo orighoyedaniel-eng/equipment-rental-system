687bfeca1ab4cedcd5c449ba4596db886ab72d18
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-16 23:55:28 +0100

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,16 @@
+// routes/user.js
+
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-router.get('/', (req, res) => res.send('User routes working'));
+// ➡️ Register a new user
+router.post('/register', userController.registerUser);
+
+// ➡️ Login user
+router.post('/login', userController.loginUser);
+
+// ➡️ Get user profile by ID
+router.get('/:id', userController.getUserProfile);
 
 module.exports = router;
