5cbe1b867017fad48c2d7b0fc63c75d9521b6a4d
orighoyedaniel-eng <orighoyedaniel@gmail.com>
2026-04-16 22:23:53 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("/workspaces/equipment-rental-system/config/database.js");
-const User = require("/workspaces/equipment-rental-system/models/User.js");
-const Equipment = require("/workspaces/equipment-rental-system/models/Equipment.js");
-const Rental = require("/workspaces/equipment-rental-system/models/Rental.js");
-
-
-sequelize.sync({ alter: true })
-  .then(() => console.log("Database synced"))
-  .catch(err => console.error("Error syncing DB:", err));
